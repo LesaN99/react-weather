ebd63ae36b386d32b95ecdeda6dc72a7e5003137
LesaN99 <149886844+LesaN99@users.noreply.github.com>
2024-01-31 14:01:48 +0200

    Injected the Api integration .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-    <Weather />
+    <Weather defaultCity= "Johannesburg" />
       <footer>
           This project was coded by
           <a href="https://github.com/LesaN99" target="_blank" rel="noreferrer"> Palesa Ndlovu </a> and is
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Weather.css"
+import axios from "axios";
 
 
-export default function Weather() {
+export default function Weather(props) {
+    const [weather, setWeather] = useState({ready: false});
+
+function showTemperature(response) {
+  setWeather({
+      ready: true,
+      city: response.data.name,
+      date: "Wednesday 14:00",
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png`,
+      description: response.data.weather[0].description,
+    });
+}
+
+if (weather.ready) {
     return (
         <div className="Weather">
             <div className="weather-app">
@@ -18,29 +35,37 @@ export default function Weather() {
           <input type="submit" value="Search" className="search-button" />
         </form>
         </header>
-            <h1>Johannesburg</h1>
+            <h1>{weather.city}</h1>
             <ul>
-                <li>Thursday 17:00</li>
-                <li>Mostly cloudy</li>
+                <li>{weather.date}</li>
+                <li className="text-capitalize">{weather.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearfix">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png" alt="isolated thunderstorms" 
+                    <img src={weather.iconUrl} alt={weather.description}
                     className="float-left" />
-                   <span className="temperature"> 26</span>
+                   <span className="temperature">{Math.round(weather.temperature)}</span>
                    <span className="unit">°C</span>
                    </div>
                 </div>
                 <div className="col-6">
                     <uL>
-                        <li>Precipitation: 5%</li>
-                        <li>Humidity: 45%</li>
-                        <li>Wind: 10 km/h</li>
+                        <li>humidity: {weather.humidity}%</li>
+                        <li>wind: {weather.wind}km/h</li> 
                     </uL>
                 </div>
             </div>
             </div>
         </div>
-    )
+    );
+
+} else {
+    const apiKey = "5354b60afda2b7800186c06153932396";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(showTemperature);
+
+    return "Loading...";
+}
+   
 }
